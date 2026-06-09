644e8fdcbabaa8b38d991a02a06dcd13e9a343ff
Ash6414 <ashw6414@gmail.com>
2026-06-09 17:09:33 -0500

    Treat AudioMoth BUSY as advisory during handshake

--- ESPBridge-MothNode1/Config.h
+++ ESPBridge-MothNode1/Config.h
@@ -71,7 +71,7 @@
 #define MOTH_UART_BAUD               9600
 #define MOTH_READY_TIMEOUT_MS        65000
 #define MOTH_LINE_TIMEOUT_MS         4000
-#define MOTH_BUSY_WAIT_MS            65000
+#define MOTH_BUSY_WAIT_MS            5000
 #define MOTH_MAX_FILES_PER_SESSION   16
 #define MOTH_CHUNK_BYTES             512
 
