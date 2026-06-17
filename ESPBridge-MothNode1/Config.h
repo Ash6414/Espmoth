dde66edfdc21638584e6317fba70faee65de20d9
Ash6414 <ashw6414@gmail.com>
2026-06-17 12:39:32 -0500

    Use stable 9600 baud for AudioMoth software UART bridge

--- ESPBridge-MothNode1/Config.h
+++ ESPBridge-MothNode1/Config.h
@@ -68,7 +68,7 @@
 // ----------------------------
 // AudioMoth bridge protocol
 // ----------------------------
-#define MOTH_UART_BAUD               115200
+#define MOTH_UART_BAUD               9600
 #define MOTH_READY_TIMEOUT_MS        65000
 #define MOTH_LINE_TIMEOUT_MS         6000
 #define MOTH_LIST_TIMEOUT_MS         10000
